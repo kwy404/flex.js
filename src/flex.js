c964ff62dda3ee34902edba7ce5226bff3e978a9
kwy404 <thekaway404@gmail.com>
2023-04-03 19:46:40 -0300

    feat: f-each

--- src/flex.js
+++ src/flex.js
@@ -7,7 +7,9 @@ class Flex {
         this.parse = new parse_();
         this.el = el;
         this.state = this.createStateProxy(state);
-        this.elements = this.getNonEmptyChildElements();
+        this.elements = Array.from(document.querySelectorAll(`${this.el} *`)).filter(
+            (element) => element.childNodes.length && /\S/.test(element.textContent)
+        );
         this.cloneNode = new html_parse_(this.el);
         this.start_app();
         this.methods_({
@@ -24,41 +26,9 @@ class Flex {
             },
         });
     }
-    
-    getNonEmptyChildElements() {
-        const elements = Array.from(document.querySelectorAll(`${this.el} *`));
-        return elements.filter((element) => {
-          if (element.nodeType === Node.TEXT_NODE) {
-            return /\S/.test(element.textContent);
-          } else {
-            const childNodes = Array.from(element.childNodes);
-            return childNodes.some((node) => {
-              if (node.nodeType === Node.TEXT_NODE) {
-                return /\S/.test(node.textContent);
-              } else if (node.nodeType === Node.ELEMENT_NODE) {
-                return node.childNodes.length && node.childNodes.some((child) => {
-                  return child.nodeType === Node.TEXT_NODE && /\S/.test(child.textContent);
-                });
-              }
-            });
-          }
-        });
-      }
-      
-      getNonEmptyChildElementsRecursively(node) {
-        const childNodes = Array.from(node.childNodes);
-        return childNodes.filter((childNode) => {
-          if (childNode.nodeType === Node.TEXT_NODE && /\S/.test(childNode.textContent)) {
-            return true;
-          } else if (childNode.nodeType === Node.ELEMENT_NODE) {
-            return this.getNonEmptyChildElementsRecursively(childNode).length > 0;
-          }
-          return false;
-        });
-      }
+
 
     start_app() {
-        console.log(this.elements)
         this.elements.forEach((element, index) => {
             if (element.nodeType === Node.TEXT_NODE) {
                 const tokens = this.parse.text(element.textContent);
@@ -128,31 +98,54 @@ class Flex {
         });
     }
 
-    methods_({
-        methods
-    }) {
+    methods_({ methods }) {
         const clickElems = Array.from(document.querySelectorAll(`${this.el} *`));
-        const nodeOld = Array.from(document.querySelectorAll(`${this.el} > *`)); // correção aqui
+        const nodeOld = Array.from(document.querySelectorAll(`${this.el} > *`));
         const flex = this;
+      
         nodeOld.forEach((elem, index) => {
-            const attrs = elem.attributes;
-            for (let i = 0; i < attrs.length; i++) {
-                const attr = attrs[i];
-                const methodName = attr.name;
-                const attrMethod = methodName.replace(':', '');
-                if (methodName && methodName[0] === ':') {
-                    const methodName_ = elem.getAttribute(methodName);
-                    clickElems[index].addEventListener(attrMethod, function() {
-                        if (typeof methods[methodName_] === 'function') {
-                            methods[methodName_].call(flex);
-                        }
-                    });
-                    elem.removeAttribute(methodName);
-                    elem.cloneNode(true);
+          const attrs = elem.attributes;
+          for (let i = 0; i < attrs.length; i++) {
+            const attr = attrs[i];
+            const methodName = attr.name;
+            const attrMethod = methodName.replace(':', '');
+            if (methodName && methodName[0] === ':') {
+              const methodName_ = elem.getAttribute(methodName);
+              clickElems[index].addEventListener(attrMethod, function () {
+                if (typeof methods[methodName_] === 'function') {
+                  methods[methodName_].call(flex);
                 }
+              });
+              elem.removeAttribute(methodName);
             }
+          }
         });
-    }
-    
-
+      
+        const elemsWithEach = Array.from(document.querySelectorAll(`${this.el} [f-each]`));
+      
+        elemsWithEach.forEach((elem) => {
+          const [item, collection] = elem.getAttribute('f-each').split(' in ');
+          const collectionArray = flex.state[collection];
+          if (collectionArray) {
+            const fragment = document.createDocumentFragment();
+            collectionArray.forEach((data, index) => {
+              const clone = elem.cloneNode(true);
+              clone.removeAttribute('f-each');
+              const innerElem = clone.querySelector('*');
+              if (innerElem) {
+                innerElem.textContent = data[item];
+              }
+              clone.textContent =collectionArray[index];
+              fragment.appendChild(clone);
+            });
+            elem.parentNode.replaceChild(fragment, elem);
+          }
+        });
+      
+        const textElems = Array.from(document.querySelectorAll(`${this.el} *[f-text]`));
+        textElems.forEach((elem) => {
+          const textKey = elem.getAttribute('f-text');
+          elem.textContent = flex.state[textKey];
+        });
+      }      
 }
