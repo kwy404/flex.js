b969cb97c999fe7fc117a6f849a6bdd09b96e913
kwy404 <thekaway404@gmail.com>
2023-04-03 19:09:01 -0300

    fix

--- src/flex.js
+++ src/flex.js
@@ -1,67 +1,101 @@
 class Flex {
     constructor({
-      el = "html",
-      state = {},
-      methods = {}
+        el = 'html',
+        state = {},
+        methods = {}
     }) {
-      this.parse = new parse_();
-      this.el = el;
-      this.state = new Proxy(state, {
-        set: (obj, prop, value) => {
-          obj[prop] = value;
-          this.update_elements();
-          return true;
-        },
-      });
-      this.elClone = document.querySelector(this.el).cloneNode(true)
-      this.elements = Array.from(document.querySelectorAll(`${this.el} *`)).filter(
-        (element) => element.childNodes.length && /\S/.test(element.textContent)
-      );
-      this.cloneNode = new html_parse_(this.el);
-      this.start_app();
-      this.methods_({
-        methods
-      });
+        this.parse = new parse_();
+        this.el = el;
+        this.state = this.createStateProxy(state);
+        this.elements = this.getNonEmptyChildElements();
+        this.cloneNode = new html_parse_(this.el);
+        this.start_app();
+        this.methods_({
+            methods
+        });
     }
-  
-    start_app() {
-      this.elements.forEach((element, index) => {
-        if (element.nodeType === Node.TEXT_NODE) {
-          const tokens = this.parse.text(element.textContent);
-          if (tokens) {
-            tokens.forEach((token) => {
-              if (token.key) {
-                element.textContent = element.textContent.replace(
-                  token.text,
-                  this.state[token.key]
-                );
+    
+    createStateProxy(state) {
+        return new Proxy(state, {
+            set: (obj, prop, value) => {
+                obj[prop] = value;
+                this.updateElements(prop);
+                return true;
+            },
+        });
+    }
+    
+    getNonEmptyChildElements() {
+        const elements = Array.from(document.querySelectorAll(`${this.el} *`));
+        return elements.filter((element) => {
+          if (element.nodeType === Node.TEXT_NODE) {
+            return /\S/.test(element.textContent);
+          } else {
+            const childNodes = Array.from(element.childNodes);
+            return childNodes.some((node) => {
+              if (node.nodeType === Node.TEXT_NODE) {
+                return /\S/.test(node.textContent);
+              } else if (node.nodeType === Node.ELEMENT_NODE) {
+                return node.childNodes.length && node.childNodes.some((child) => {
+                  return child.nodeType === Node.TEXT_NODE && /\S/.test(child.textContent);
+                });
               }
             });
           }
-        } else {
-          Array.from(element.childNodes).forEach((childNode) => {
-            if (childNode.nodeType === Node.TEXT_NODE) {
-              const tokens = this.parse.text(childNode.textContent);
-              if (tokens) {
-                tokens.forEach((token) => {
-                  if (token.key) {
-                    childNode.textContent = childNode.textContent.replace(
-                      token.text,
-                      this.state[token.key]
-                    );
-                  }
+        });
+      }
+      
+      getNonEmptyChildElementsRecursively(node) {
+        const childNodes = Array.from(node.childNodes);
+        return childNodes.filter((childNode) => {
+          if (childNode.nodeType === Node.TEXT_NODE && /\S/.test(childNode.textContent)) {
+            return true;
+          } else if (childNode.nodeType === Node.ELEMENT_NODE) {
+            return this.getNonEmptyChildElementsRecursively(childNode).length > 0;
+          }
+          return false;
+        });
+      }
+
+    start_app() {
+        console.log(this.elements)
+        this.elements.forEach((element, index) => {
+            if (element.nodeType === Node.TEXT_NODE) {
+                const tokens = this.parse.text(element.textContent);
+                if (tokens) {
+                    tokens.forEach((token) => {
+                        if (token.key) {
+                            element.textContent = element.textContent.replace(
+                                token.text,
+                                this.state[token.key]
+                            );
+                        }
+                    });
+                }
+            } else {
+                Array.from(element.childNodes).forEach((childNode) => {
+                    if (childNode.nodeType === Node.TEXT_NODE) {
+                        const tokens = this.parse.text(childNode.textContent);
+                        if (tokens) {
+                            tokens.forEach((token) => {
+                                if (token.key) {
+                                    childNode.textContent = childNode.textContent.replace(
+                                        token.text,
+                                        this.state[token.key]
+                                    );
+                                }
+                            });
+                        }
+                    }
                 });
-              }
             }
-          });
-        }
-      });
+        });
     }
-  
-    update_elements() {
+
+    updateElements(prop) {
         const elActual = document.querySelectorAll(`${this.el} *`);
-        const elClone = this.elClone.cloneNode(true);
-    
+        const elClone = this.cloneNode.cloneNode(true);
+
         Array.from(elClone.querySelectorAll('*')).forEach((element, index) => {
             const targetElement = elActual[index];
             if (element.nodeType === Node.TEXT_NODE) {
@@ -93,30 +127,32 @@ class Flex {
             }
         });
     }
-    
-      
-    methods_({ methods }) {
+
+    methods_({
+        methods
+    }) {
         const clickElems = Array.from(document.querySelectorAll(`${this.el} *`));
-        const nodeOld = Array.from(document.querySelectorAll(`${this.el} *`)); // correção aqui
+        const nodeOld = Array.from(document.querySelectorAll(`${this.el} > *`)); // correção aqui
         const flex = this;
-        nodeOld.forEach((elem,index) => {
-          const attrs = elem.attributes;
-          for (let i = 0; i < attrs.length; i++) {
-            const attr = attrs[i];
-            const methodName = attr.name;
-            const attrMethod = methodName.replace(':', '');
-            if (methodName && methodName[0] === ':') {
-              const methodName_ = elem.getAttribute(methodName);
-              clickElems[index].addEventListener(attrMethod, function () {
-                if (typeof methods[methodName_] === 'function') {
-                  methods[methodName_].call(flex);
+        nodeOld.forEach((elem, index) => {
+            const attrs = elem.attributes;
+            for (let i = 0; i < attrs.length; i++) {
+                const attr = attrs[i];
+                const methodName = attr.name;
+                const attrMethod = methodName.replace(':', '');
+                if (methodName && methodName[0] === ':') {
+                    const methodName_ = elem.getAttribute(methodName);
+                    clickElems[index].addEventListener(attrMethod, function() {
+                        if (typeof methods[methodName_] === 'function') {
+                            methods[methodName_].call(flex);
+                        }
+                    });
+                    elem.removeAttribute(methodName);
+                    elem.cloneNode(true);
                 }
-              });
-              elem.removeAttribute(methodName);
-              elem.cloneNode(true);
             }
-          }
         });
-      }
-      
-}  
+    }
+    
+
+}
