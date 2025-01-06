852ecf8e6d2e598efce13295bc5be859c7c9b7fc
kwy404 <thekaway404@gmail.com>
2025-01-06 09:28:35 -0300

    fix: each

--- src/directives/each.js
+++ src/directives/each.js
@@ -6,33 +6,40 @@ function eachLoop(element, state, targetElement) {
       const [itemVar, arrayVar] = eachAttr.split(' in ');
       const array = state[arrayVar];
       if (!Array.isArray(array)) return;
-      element.removeAttribute(ATTR_EACH)
+      element.removeAttribute(ATTR_EACH);
       const parentElement = element.parentElement;
       const cloneNode = element.cloneNode(true);
       element.innerHTML = '';
       const element_ = document.createElement(parentElement.tagName).cloneNode(true);
       array.forEach((item) => {
         const newElement = cloneNode.cloneNode(true);
-        newElement.innerHTML = newElement.innerHTML.replace(new RegExp(`\\[\\s*${itemVar}\\s*\\]`, 'g'), item);
+        if (typeof item === 'object') {
+          let innerHTML = newElement.innerHTML;
+          for (const key in item) {
+            if (item.hasOwnProperty(key)) {
+              innerHTML = innerHTML.replace(new RegExp(`\\[\\s*${itemVar}\\.${key}\\s*\\]`, 'g'), item[key]);
+            }
+          }
+          newElement.innerHTML = innerHTML;
+        } else {
+          newElement.innerHTML = newElement.innerHTML.replace(new RegExp(`\\[\\s*${itemVar}\\s*\\]`, 'g'), item);
+        }
         element_.appendChild(newElement);
       });
-        const tokens = parseText().text(element_.innerHTML);
-        let novoHTML = ""
-        if (tokens) {
-          tokens.forEach((token) => {
-            if (token.key) {
-              novoHTML = element_.innerHTML.replaceAll(
-                token.text,
-                state[token.key]
-              );
-            }
-          })
-        }
-        targetElement.innerHTML = novoHTML
-        if (element.innerHTML !== targetElement.innerHTML) {
-          targetElement.innerHTML = novoHTML;
-        }
-        return;
+      const tokens = parseText().text(element_.innerHTML);
+      let novoHTML = element_.innerHTML;
+      if (tokens) {
+        tokens.forEach((token) => {
+          if (token.key) {
+            novoHTML = novoHTML.replaceAll(token.text, state[token.key]);
+          }
+        });
       }
+      targetElement.innerHTML = novoHTML;
+      if (element.innerHTML !== targetElement.innerHTML) {
+        targetElement.innerHTML = novoHTML;
+      }
+      return;
     }
   }
+}
